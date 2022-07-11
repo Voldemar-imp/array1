fd022b2fb8ef93ce695952d8922b120e52335735
Voldemar-imp <108013210+Voldemar-imp@users.noreply.github.com>
2022-07-11 18:23:41 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,31 +10,35 @@ namespace array
     {
         static void Main(string[] args)
         {
-            int [,] numbersArray = { { 4, 3 ,6 , 9}, { 3, 4, 9, 1 }, { 5, 6 , 7, 8} };
-            int sumLine =0;
+            int[,] numbersArray = { { 4, 3, 6, 9 }, { 3, 4, 9, 1 }, { 5, 6, 7, 8 } };
+            int sumLine = 0;
             int productColumn = 1;
             int numnerOfLine = 2;
             int numnerOfColumn = 1;
 
             for (int i = 0; i < numbersArray.GetLength(0); i++)
-            {                
-                    productColumn *= numbersArray[i, numnerOfColumn-1];
-               
+            {
+                productColumn *= numbersArray[i, numnerOfColumn - 1];        
+            }
+
+            for (int i = 0; i < numbersArray.GetLength(1); i++)
+            {
+                sumLine += numbersArray[numnerOfLine-1, i];                
+            } 
+
+            for (int i = 0; i < numbersArray.GetLength(0); i++)
+            {
+                
                 for (int j = 0; j < numbersArray.GetLength(1); j++)
                 {
-                    if (i == numnerOfLine - 1)
-                    {
-                        sumLine += numbersArray[i, j];
-                    }
-                    
-                    Console.Write(numbersArray[i, j] + " ");
+                  Console.Write(numbersArray[i, j] + " ");
                 }
-                
+
                 Console.WriteLine();
             }
-            
+
             Console.WriteLine(sumLine + " - сумма 2-й строки матрицы");
-            Console.WriteLine(productColumn+" - произведение 1-го столбца матрицы");
+            Console.WriteLine(productColumn + " - произведение 1-го столбца матрицы");
         }
     }
 }
